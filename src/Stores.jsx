e5798b76ee6b1599b0d90fb0592218538a51cf7c
pero786 <sarecp3@gmail.com>
2024-11-11 08:54:03 +0100

    storeelement

--- src/Stores.jsx
+++ src/Stores.jsx
@@ -3,46 +3,66 @@ import { createStore } from "solid-js/store";
 
 export default function Stores() {
 
-    const [db1, setDb1] = createSignal({
-        name: "users",
-        items: [
-            {name:"Pero", surname:"Perić", bill:100},
-            {name:"Mare", surname:"Perica", bill:20},
-            {name:"Ivo", surname:"Perić", bill:300},
-            {name:"Viktor", surname:"Ivić", bill:500},
-        ]
+  const [db1, setDb1] = createSignal({
+    name: "users",
+    items: [
+      { name: "Pero", surname: "Perić", bill: 100 },
+      { name: "Mare", surname: "Perica", bill: 20 },
+      { name: "Ivo", surname: "Perić", bill: 300 },
+      { name: "Viktor", surname: "Ivić", bill: 500 },
+    ],
+  });
+
+  const [db2, setDb2] = createStore({
+    name: "users",
+    items: [
+      { name: "Matija", surname: "Perić", bill: 100 },
+      { name: "Saša", surname: "Perica", bill: 20 },
+      { name: "Tin", surname: "Perić", bill: 300 },
+      { name: "Vanja", surname: "Ivić", bill: 500 },
+    ],
+  });
+
+  function applyTax1() {
+    const newItems = db1().items.map((element) => {
+      const updatedElement = { ...element, bill: element.bill * 1.25 };
+      return updatedElement;
     });
+    setDb1({ ...db1(), items: newItems });
+  }
 
-    const [db2, setDb2] = createStore();
+  function applyTax2() {
+    const newItems = db2.items.map((element) => {
+      return { ...element, bill: element.bill * 1.25 };
+    });
+    setDb2("items", newItems); 
+  }
 
-    function applyTax(){
-        let newItems = [];
-        for (let i = 0; i < db1(). items.length; i++){
-            let element = {...db1().items[i]};
-            element.bill *= 1.25;
-            newItems.push(element);
-        }
-        let newObject = {
-            ...db1(),
-            ...{ items: newItems}
-        }
-        setDb1([]);
-        setDb1(newObject);
-    }
+  return (
+    <>
+      <h1>Korisnici</h1>
+      <For each={db1().items}>
+        {(item) => (
+          <div style="margin-bottom: 10px; padding:10px;border:1px solid grey">
+            <div>Ime: {item.name}</div>
+            <div>Prezime: {item.surname}</div>
+            <div>Račun: {item.bill} EUR</div>
+          </div>
+        )}
+      </For>
+      <button onClick={applyTax1}>Uračunaj porez</button>
 
-    return(
-        <>
-        <h1>Korisnici</h1>
-        <For each  = {db1().items}>
-            {item => (
-                <div style="margin-bottom: 10px; padding:10px;border:1px solid grey">
-                    <div>Ime: {item.name}</div>
-                    <div>Prezime: {item.surname}</div>
-                    <div>Račun: {item.bill}</div>
-                </div>
-            )}
-        </For>
-        <button onClick={applyTax}>Uračunaj porez</button>
-        </>
-    )
+      <h1>Korisnici verzija 2</h1>
+      <For each={db2.items}>
+        {(item) => (
+          <div style="margin-bottom: 10px; padding:10px;border:1px solid grey">
+            <div>Ime: {item.name}</div>
+            <div>Prezime: {item.surname}</div>
+            <div>Račun: {item.bill} EUR</div>
+          </div>
+        )}
+      </For>
+      <button onClick={applyTax2}>Uračunaj porez</button>
+    </>
+  );
 }
